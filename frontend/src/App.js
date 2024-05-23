d31e6d69d3ceb12da0e6e33ee6b438cc4fa5fec0
Andrew Lin <andrew.mlin27@gmail.com>
2024-05-23 04:37:48 -0500

    buy/sell buttons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import Home from './components/Home';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Schedule from './components/Schedule';
+import Buy from './components/Buy';
+import Sell from './components/Sell';
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/schedule" element={<Schedule/>} />
+        <Route path="/buy" element={<Buy/>} />
+        <Route path="/sell" element={<Sell/>} />
       </Routes>
 
       <Footer/>
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,17 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './css/Home.css';
 
 const Home = () => {
   return (
     <div className="Home">
-    <header className="Home-header">
-      <h1 className="title">Aggie Sports Pass</h1>
-      <div className="button-container">
-        <button className="btn btn-buy">Buy Now</button>
-        <button className="btn btn-sell">Sell Now</button>
-      </div>
-    </header>
-  </div>
+      <header className="Home-header">
+
+        <h1 className="title">Aggie Sports Pass</h1>
+
+        <div className="button-container">
+          <Link to="/buy"> <button className="btn btn-buy">Buy Now</button> </Link>
+          <Link to="/sell"> <button className="btn btn-sell">Sell Now</button> </Link>
+        </div>
+
+      </header>
+    </div>
   )
 }
 
