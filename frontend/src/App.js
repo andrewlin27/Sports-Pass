f36297bd427bb919f57874186e47bfcea9310d90
Siddharth Venkatraman <sid.venkatraman17@gmail.com>
2024-05-23 14:54:35 -0500

    Added transitions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,37 @@
 import React from 'react';
 import './App.css';
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Schedule from './components/Schedule';
 import Buy from './components/Buy';
 import Sell from './components/Sell';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
-function App() {
+const App = () => {
+  const location = useLocation();
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar/>
-
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/schedule" element={<Schedule/>} />
-          <Route path="/buy" element={<Buy/>} />
-          <Route path="/sell" element={<Sell/>} />
-        </Routes>
-
-        <Footer/>
-      </BrowserRouter>
+      <Navbar />
+      <TransitionGroup>
+        <CSSTransition
+          key={location.key}
+          timeout={300}
+          classNames="fade"
+        >
+          <Routes location={location}>
+            <Route path="/" element={<Home />} />
+            <Route path="/schedule" element={<Schedule />} />
+            <Route path="/buy" element={<Buy />} />
+            <Route path="/sell" element={<Sell />} />
+          </Routes>
+        </CSSTransition>
+      </TransitionGroup>
+      <Footer />
     </div>
   );
 }
+
 export default App;
