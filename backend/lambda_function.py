e73c7e38c8746d5611139986be83aaa2ead7ffc1
Andrew Lin <andrew.mlin27@gmail.com>
2024-05-31 05:43:11 -0500

    delete functional

--- backend/lambda_function.py
+++ backend/lambda_function.py
@@ -1,10 +1,9 @@
 import json
 import boto3
 from botocore.exceptions import ClientError
+from boto3.dynamodb.conditions import Key
 from datetime import datetime, timedelta, timezone
 
-# tz = pytz.timezone('America/Chicago')
-# timestamp = datetime.now(tz).strftime('%Y-%m-%d %H:%M:%S.%f')
 
 dynamodb = boto3.resource('dynamodb')
 dynamodb_table = dynamodb.Table('SP_posts')
@@ -19,26 +18,27 @@ def lambda_handler(event, context):
 
         if http_method == 'GET':
             if event['queryStringParameters']:
-                response = get_post(event['queryStringParameters']['name'])
+                if 'name' in event['queryStringParameters']:
+                    response = get_posts_by_name(event['queryStringParameters']['name'])
+                elif 'timestamp' in event['queryStringParameters']:
+                    response = get_post_by_timestamp(event['queryStringParameters']['timestamp'])
             else:
                 response = get_all_posts()
 
         elif http_method == 'POST':
-            time = datetime.now(timezone.utc)
-            cstOffset = timedelta(hours=-5)
-
             body = json.loads(event['body'])
-            body['timestamp'] = str(time + cstOffset)[:-6]
-            
-            response = save_employee(body)
+            response = post_post(body)
 
         elif http_method == 'PATCH':
             body = json.loads(event['body'])
             response = modify_employee(body['employeeId'], body['updateKey'], body['updateValue'])
 
         elif http_method == 'DELETE':
-            body = json.loads(event['body'])
-            response = delete_employee(body['employeeId'])
+            timestamp = event['queryStringParameters']['timestamp']
+            name = event['queryStringParameters']['name']
+
+            response = delete_employee(timestamp, name)
+
         else:
             response = build_response(404, '404 Not Found')
 
@@ -48,11 +48,38 @@ def lambda_handler(event, context):
    
     return response
 
-def get_post(employee_id):
+def get_posts_by_name(name):
     try:
-        response = dynamodb_table.get_item(Key={'name': employee_id})
-        return build_response(200, response.get('Item'))
+        response = dynamodb_table.query(
+            IndexName='name-index',
+            KeyConditionExpression=Key('name').eq(name)
+        )
+        data = response.get('Items', [])
         
+        
+        while 'LastEvaluatedKey' in response:
+            response = dynamodb_table.query(
+                IndexName='name-index',
+                KeyConditionExpression=Key('name').eq(name),
+                ExclusiveStartKey=response['LastEvaluatedKey']
+            )
+            data.extend(response.get('Items', []))
+        
+        return build_response(200, data)
+        
+    except ClientError as e:
+        print('Error:', e)
+        return build_response(400, e.response['Error']['Message'])
+    
+def get_post_by_timestamp(timestamp):
+    try:
+        response = dynamodb_table.query(
+            KeyConditionExpression=Key('timestamp').eq(timestamp)
+        )
+        data = response.get('Items', [])
+        
+        return build_response(200, data[0])
+    
     except ClientError as e:
         print('Error:', e)
         return build_response(400, e.response['Error']['Message'])
@@ -62,7 +89,6 @@ def get_all_posts():
         response = dynamodb_table.scan()
         data = response.get('Items', [])
         
-        # Continue scanning if there are more pages of data
         while 'LastEvaluatedKey' in response:
             response = dynamodb_table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
             data.extend(response.get('Items', []))
@@ -73,15 +99,16 @@ def get_all_posts():
         print('Error:', e)
         return build_response(400, e.response['Error']['Message'])
 
-def save_employee(request_body):
+def post_post(request_body):
     try:
+        time = datetime.now(timezone.utc)
+        cstOffset = timedelta(hours=-5)
+        request_body['timestamp'] = str(time + cstOffset)[:-6]
+
         dynamodb_table.put_item(Item=request_body)
-        body = {
-            'Item': request_body,
-            'Message': 'SUCCESS',
-            'Operation': 'POST'
-        }
-        return build_response(200, body)
+
+        return build_response(200, request_body)
+    
     except ClientError as e:
         print('Error:', e)
         return build_response(400, e.response['Error']['Message'])
@@ -104,18 +131,33 @@ def modify_employee(employee_id, update_key, update_value):
         print('Error:', e)
         return build_response(400, e.response['Error']['Message'])
 
-def delete_employee(employee_id):
+def delete_employee(timestamp, name):
+    
     try:
         response = dynamodb_table.delete_item(
-            Key={'employeeid': employee_id},
+            # for composite primary key, need both partition and sort key. for simple primary key, only partition key
+            # could remove name as sort key so that can use path parameter
+            Key={
+                'timestamp': timestamp,
+                'name': name
+            },
             ReturnValues='ALL_OLD'
         )
-        body = {
-            'Operation': 'DELETE',
-            'Message': 'SUCCESS',
-            'Item': response
-        }
-        return build_response(200, body)
+
+        # Item was found and deleted
+        if 'Attributes' in response:
+            body = {
+                'Item': response['Attributes'],
+                'Operation': 'DELETE',
+                'Message': 'SUCCESS'
+            }
+            return build_response(200, body)
+        
+        else:
+            body ={'Error': 'Item not found'}
+
+            return build_response(404, body)
+    
     except ClientError as e:
         print('Error:', e)
         return build_response(400, e.response['Error']['Message'])
@@ -123,8 +165,6 @@ def delete_employee(employee_id):
 def build_response(status_code, body):
     return {
         'statusCode': status_code,
-        'headers': {
-            'Content-Type': 'application/json'
-            },
-        'body': json.dumps(body)
+        'body': json.dumps(body),
+        'headers': {'Content-Type': 'application/json'}
     }
