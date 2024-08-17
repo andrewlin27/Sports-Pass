a7be7800571a1911b8b750c8df078041a2539a56
Siddharth Venkatraman <sid.venkatraman17@gmail.com>
2024-08-17 17:32:22 -0500

    worked on resizing

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -17,28 +17,28 @@ const Card = ({ id, seller, price, classification, postingDate, game, image, pho
     }
   };
 
-  const calculateClassification = (classification) => {
-    switch (classification) {
-      case 'Freshman':
-        return 'U1';
-      case 'Sophomore':
-        return 'U2';
-      case 'Junior':
-        return 'U3';
-      case 'Senior':
-        return 'U4';
-      default:
-        return 'Unknown'; // Optional: handle cases where the classification doesn't match any of the known values
-    }
-  };
+  // const calculateClassification = (classification) => {
+  //   switch (classification) {
+  //     case 'Freshman':
+  //       return 'U1';
+  //     case 'Sophomore':
+  //       return 'U2';
+  //     case 'Junior':
+  //       return 'U3';
+  //     case 'Senior':
+  //       return 'U4';
+  //     default:
+  //       return 'Unknown'; // Optional: handle cases where the classification doesn't match any of the known values
+  //   }
+  // };
 
   return (
     <div className="card" onClick={onClick}>
       <div className="card-date-badge">{calculateDateDifference(postingDate)}</div>
       <img src={`images/${image}`} alt={`${seller}'s product`} className="card-img" />
       <div className="card-content">
-        <h2 className="card-price">${price}</h2>
-        <p className="card-description">{calculateClassification(classification)} Sports Pass</p>
+        <h3 className="card-price">${price}</h3>
+        <p className="card-description">{classification}</p>
       </div>
     </div>
   );
