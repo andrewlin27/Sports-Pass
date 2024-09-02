fdc34991cd631ada46e9b012b5db1cf96c549428
Andrew Lin <andrew.mlin27@gmail.com>
2024-09-02 02:09:02 -0500

    rebrand to Aggie Passes

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div className="Home">
       <header className="Home-header">
-        <h1 className="title"> AggiePasses </h1>
+        <h1 className="title"> Aggie Passes </h1>
         <div className="button-container">
           <Link to="/buy"> <button className="btn btn-buy">Buy Now</button> </Link>
           <Link to="/sell"> <button className="btn btn-sell">Sell Now</button> </Link>
