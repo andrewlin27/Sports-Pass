5c587c786e9793198939030cc1dd8fd5c47af205
Andrew Lin <andrew.mlin27@gmail.com>
2024-09-02 02:48:53 -0500

    minor changes

--- frontend/src/components/Info.js
+++ frontend/src/components/Info.js
@@ -4,7 +4,7 @@ import './css/Info.css'
 const Info = () => {
   return (
     <div className='info-container'>
-        <img src='images/guest.png' alt='guest tickets'/>
+        <img src='images/guest2.png' alt='guest tickets'/>
         <img src='images/schedule.png' alt='schedule'/>
         <img src='images/ticketPull.png' alt='pull schedule'/>
     </div>
